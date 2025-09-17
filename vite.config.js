bca7a1e2163cd78e663ed13e46e185b8f013feae
Kaan Yeşil <thannus611@gmail.com>
2025-09-18 01:55:53 +0300

    Fix single export and syntax in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-export default defineConfig({d
-  plugins: [react()],
-})
 export default defineConfig({
   plugins: [
     react(),
